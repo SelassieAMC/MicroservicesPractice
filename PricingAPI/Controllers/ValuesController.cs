0b098840540990fb94614f35007c216a3a8e8321
Andres Mauricio Clavijo Mejia <aclavijo@belatrix.com>
2020-05-27 12:01:19 -0500

    Initial commit, base project connected UI and API with Polly for resilience

---
+++ PricingAPI/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace PricingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2", Environment.MachineName };
+        }
+    }
+}
---
+++ Store/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Store.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly HttpClient _client;
+
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _client = httpClientFactory.CreateClient("pricing");
+        }
+
+        public string[] Values { get; set; }
+
+        public async Task OnGet()
+        {
+            var response = await _client.GetAsync("/api/values");
+            var content = await response.Content.ReadAsStringAsync();
+            Values = JsonConvert.DeserializeObject<string[]>(content);
+        }
+    }
+}
